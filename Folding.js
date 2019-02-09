228e5c05267a1b0a86f429a825d66d786db6a221
Adventure <m15583730895@163.com>
2019-02-09 15:24:38 +0800

    readme

---
+++ Folding.js
@@ -0,0 +1,43 @@
+(function($) {
+    $.fn.Folding = function(options) {
+        return this.each(function() {
+            var settings = $.extend({
+                size: 40,
+                id: undefined,
+                link: undefined,
+                background: '#fff' 
+            }, options);
+
+            var id = settings.id === undefined ? '' : ' id="' 
+                    + settings.id + '"';
+            var a_start = settings.link === undefined ? '' : '<a href="'
+                    + settings.link + '" target="_blank">';
+            var a_end = settings.link === undefined ? '' : '</a>';
+            var element = '<div' + id + ' class="Folding" style="width: '
+                    + settings.size * 2 + 'px; height: ' + settings.size
+                    + 'px; position: absolute; bottom: 0; right: 0;">' + a_start
+                    + '<div class="Folding_left" style="border-width: 0 0 '
+                    + settings.size + 'px ' + settings.size 
+                    + 'px; width: 0px; height: 0px; border-style: solid; '
+                    + 'border-color: transparent transparent #999 transparent; '
+                    + 'opacity: 0.6; float: left; z-index: 10;"></div>'
+                    + '<div class="Folding_square" style="border-width: 0 0 '
+                    + settings.size + 'px ' + settings.size 
+                    + 'px; border-color: transparent transparent ' 
+                    + settings.background 
+                    + ' transparent; width: 0px; height: 0px; '
+                    + 'border-style: solid; float: left;"></div>'
+                    + '<div class="Folding_right" '
+                    + 'style="border-width: ' + settings.size + 'px ' 
+                    + settings.size + 'px 0 0; left: ' + settings.size 
+                    + 'px; top: -' + settings.size + 'px; width: 0px; '
+                    + 'height: 0px; border-style: solid; '
+                    + 'border-color: #fff transparent transparent transparent; '
+                    + 'position: relative; opacity: 0.6; float: left; '
+                    + 'z-index: 10;"></div>' 
+                    + a_end;
+
+            return $(this).append(element).css('position', 'relative');
+        });
+    }
+}(jQuery));
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# Jquery-Folding-angle
-这是一个jQuery插件，可以在右下角创建平面设计的纸张折叠效果，这对于时尚和链接提示都很有用。它使用CSS边框技巧来实现效果，而不使用任何图像，这使我们能够创建任何大小的角落。了解如何实现这一目标。
+## **项目介绍**
+
+- ✌这是一个**jQuery插件**，可以在右下角创建平面设计的纸张折叠效果，这对于时尚和链接提示都很有用。
+- 它使用CSS边框技巧来实现效果，而不使用任何图像，这使我们能够创建任何大小的角落。了解如何实现这一目标。😁
+
+### **如何使用**
+要使用Folding，应始终包含以下文件。
+
+    <script src="http://code.jquery.com/jquery-1.10.2.min.js" type="text/javascript"></script>
+    <script src="Folding.min.js" type="text/javascript"></script>
+
+
+
+HTML: 
+
+    <div id="paper"></div>
+
+JS:
+
+    $('#paper').Folding();
+
+假设我们想在HTML中为带有id纸的元素添加一个角，Folding使它变得如此简单：
+
+
+
+### **项目大小**
+
+大小只有1.2kb😍
+
+### **项目预览**
+
+![test_img](./image/1.png)
