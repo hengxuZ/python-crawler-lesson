149f5d5241b27f7aa5846a310e7765ec1116ff15
hengxuZ <m15583730895@163.com>
2019-11-16 10:36:28 +0800

    使用urllib模块

---
+++ lesson-01/README.md
@@ -0,0 +1,10 @@
+
+## urllib模块
+
+### urllib.parse
+> 解析模块
+
+### urllib.request
+> 请求模块
+
+[1][https://docs.python.org/zh-cn/3/library/urllib.html]
---
+++ lesson-01/urllib模块模仿百度get请求.py
@@ -0,0 +1,33 @@
+
+import urllib.request
+# 解析中文
+import urllib.parse   
+import string
+
+def get_params():
+  url = "http://www.baidu.com/s?"
+
+  params = {
+    "wd":"中文",
+    "key":' zhang',
+    "value":'san'
+  }
+  # 将字典类参数转化为 字符串参数 如：wd：”中文“ -> wd=”编码“
+  str_params = urllib.parse.urlencode(params)
+  # 连接url
+  final_url = url + str_params
+  # 带有中文的url转译成计算机可以识别的url
+  end_url = urllib.parse.quote(final_url,safe=string.printable)
+  # 开始请求
+  response = urllib.request.urlopen(end_url)
+  # # 解析响应结果
+  return response.read().decode("utf-8")
+  
+print(get_params())
+
+
+# urllib --- URL 处理模块
+# 网址 https://docs.python.org/zh-cn/3/library/urllib.html
+
+
+
